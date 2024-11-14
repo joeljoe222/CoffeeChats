06d48db0bc7c3b11bed0ff299bcbfb2b740cca12
Joel Thomas <joelkthomas99@gmail.com>
2024-11-13 18:57:09 -0600

    initial commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require("express") // instance of framework express
+const app = express() // instance of const express
+
+// listening on port 3001
+app.listen(3001,() => {
+    console.log("Server running on port 3001");
+});
+
+// database
