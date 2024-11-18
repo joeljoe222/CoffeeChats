23bce1240cdd1351d26460b166695a3dbd5e118e
Joel Thomas <joelkthomas99@gmail.com>
2024-11-18 12:02:33 -0600

    renaming index.js to app.js

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const app = express() // instance of const express
 
 // listening on port 3001
 app.listen(3001,() => {
-    console.log("Server running on port 3001");
+    console.log("Server running of port 3001");
 });
 
 // database
