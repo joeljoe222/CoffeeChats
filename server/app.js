1be237f37f9443ded9370da42df1c23ff03ac30a
Joel Thomas <joelkthomas99@gmail.com>
2024-11-27 08:47:49 -0600

    some small changes

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
-const express = require("express") // instance of framework express
-const app = express() // instance of const express
+const express = require("express"); // instance of framework express
+const app = express(); // instance of const express
+const port = 3001;
+
+app.get("/",(req,res)=>{
+    res.send(home.html);
+})
 
 // listening on port 3001
-app.listen(3001,() => {
-    console.log("Server running of port 3001");
+app.listen(port,() => {
+    console.log(`Example app listening on port http://127.0.0.1:${port}/`);
 });
 
 // database
